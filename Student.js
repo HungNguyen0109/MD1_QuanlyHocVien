80588ab75ccab2f7e7fdc14ad9591d6981e4370a
Hung <nguyenkhachung0708@gmail.com>
2022-03-11 14:17:46 +0700

    done

---
+++ Student.js
@@ -0,0 +1,25 @@
+class Student {
+    constructor(id, name, age) {
+        this.id = id
+        this.name = name
+        this.age = age
+    }
+    getID() {
+        return this.id
+    }
+    setID(id) {
+        this.id = id;
+    }
+    getName() {
+        return this.name
+    }
+    setName(name) {
+        this.name = name;
+    }
+    getAge() {
+        return this.age
+    }
+    setAge(age) {
+        this.age = age;
+    }
+}
